08079b0de9a1bd2223d2a745d2f050323caf41be
pbisaria007 <44259814+pbisaria007@users.noreply.github.com>
2019-10-10 21:15:06 +0530

    Update pravesh.py

--- data/pravesh.py
+++ data/pravesh.py
@@ -1,4 +1,4 @@
 name="Pravesh"
 year="Third"
 branch="Computer Science"
-print("Hello! I am +name+ from +year+ year +branch+ and today's Hactoberfest was fun!!!!  ")
+print("Hello! I am "+name+" from "+year+" year "+branch+" and today's Hactoberfest event was fun!!!!  ")
