0d79006981cda7a1753b518ba8e1b36d3640fbd8
pbisaria007 <44259814+pbisaria007@users.noreply.github.com>
2019-10-10 21:13:33 +0530

    Update and rename pravesh.py to data/pravesh.py

---
+++ data/pravesh.py
@@ -0,0 +1,4 @@
+name="Pravesh"
+year="Third"
+branch="Computer Science"
+print("Hello! I am +name+ from +year+ year +branch+ and today's Hactoberfest was fun!!!!  ")
--- pravesh.py
+++
@@ -1,1 +0,0 @@
-print("I love Open Source")
