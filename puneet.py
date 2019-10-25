6b7cf37a36c3c3cba4ca7483da406e4374a9817d
divyanshutripathy <56475030+divyanshutripathy@users.noreply.github.com>
2019-10-25 15:30:50 +0530

    Update puneet.py

--- puneet.py
+++ puneet.py
@@ -1,4 +1,4 @@
 name="Puneet"
 age="18"
 food_i_like="kheer,chole bhature,ras malai"
-print("i am",name,"my age is",age,"my favourate food is",food_i_like)
+print("i am",name,"my age is",age,"my favorate food is",food_i_like)
