8a7089958ebbd7ea97924c72eed47fa6ea215a72
Rohit Jakhar <rohitjakhar940@gmail.com>
2020-10-22 10:30:48 +0530

    Add My Profile in Python Code
    I write my profile in Python code using format string

---
+++ data/RohitJakhar.py
@@ -0,0 +1,6 @@
+name = "Rohit Jakhar"
+branch = "Computer Science"
+year = "3rd Year"
+print(f"Hello Everyone, My Name is {name} and my branch is {branch} in {year}.", end = ' ')
+print("Hacktoberfest is Best Event and I attend it first Time.")
+print("Thanks Digital Ocean for this Event.")
