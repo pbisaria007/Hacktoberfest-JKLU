407802c7a24fc51179c0b630d8d2bef5cf2a347c
spinder96 <spinder@mail.usf.edu>
2020-10-28 17:57:31 -0400

    Create spinder96.py

---
+++ spinder96.py
@@ -0,0 +1,6 @@
+{
+    name: "Your Name"
+    branch: "Your Branch"
+    year: "Your current year"
+    print("Print your experince of Hactoberfest event using above information in any sequece using all the above variables")
+}
