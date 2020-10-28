9646cd19cea791bb9e27eafdd86325bf0a1030be
spinder96 <spinder@mail.usf.edu>
2020-10-28 18:03:22 -0400

    Delete spinder96.py

--- spinder96.py
+++
@@ -1,6 +0,0 @@
-{
-    name: "Your Name"
-    branch: "Your Branch"
-    year: "Your current year"
-    print("Print your experince of Hactoberfest event using above information in any sequece using all the above variables")
-}
